4d1996982789466a2cacfa60c65e30c129ca4d18
Krhistopher <krissiguenza@gmail.com>
2021-05-17 19:52:53 -0600

    Version 1.0.3
    El programa esta listo

--- EnsayoA/EnsayoA.cpp
+++ EnsayoA/EnsayoA.cpp
@@ -1,21 +1,27 @@
-// EnsayoA.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 // creando el pull request
 // numeros alrevez 
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string numero, numero2 = "";
+    cout << "Ingrese el numero estraborico:" << endl;
+    cin >> numero;
+    for (int i = 0; i < numero.length(); i++)
+    {
+        numero2.append(numero.substr(numero.length()- 1 - i, 1));
+    }
+    cout << numero2 << endl;
+    if (numero == numero2)
+    {
+        cout << "El numero es estraborico" << endl;
+    }
+    else
+    {
+        cout << "El numero no es estraborico" << endl;
+    }
+
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
